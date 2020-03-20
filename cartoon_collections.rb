b58096f25a519d49a13949158555aa73a8c7fe3a
Steven Kandow <kando1sr@gmail.com>
2020-03-20 16:27:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,12 +13,13 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(words)
+  verdict = 0
   words.each do |str|
     if str.length > 4
-      verdict = 1 
+      verdict += 1 
     end 
   end
-  verdict ? true : false 
+  verdict > 0 ? true : false 
 end
 
 def find_the_cheese# code an argument here
