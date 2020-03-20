9e572d44d6790e9fec9fa5d582dc15234e8a2b82
Steven Kandow <kando1sr@gmail.com>
2020-03-20 16:33:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def long_planeteer_calls(words)
   verdict > 0 ? true : false 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
